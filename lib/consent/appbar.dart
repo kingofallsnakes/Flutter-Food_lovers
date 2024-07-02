2ada523fb0842027d41d89beae92a9a2edfccce6
keerthi yadav chirra <keerthiyadavchirra@gmail.com>
2024-07-02 12:42:17 +0530

    first commit

---
+++ lib/consent/appbar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/consent/colors.dart';
+
+PreferredSizeWidget appbar() {
+  return AppBar(
+    automaticallyImplyLeading: false,
+    elevation: 0,
+    title: Icon(
+      Icons.menu,
+      size: 27,
+    ),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 15),
+        child: CircleAvatar(
+          radius: 18,
+          backgroundImage: AssetImage('images/p4.png'),
+        ),
+      ),
+    ],
+    backgroundColor: maincolor,
+  );
+}
---
+++ lib/consent/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color background = Color(0xff76c5c0);
+Color maincolor = Color(0xff68bd32);
+Color font = Color(0xfff3C444C);
